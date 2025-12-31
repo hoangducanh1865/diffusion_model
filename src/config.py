f9e2f95fa0c87d8e91c6a1d8e65e12fc04d97623
hoangducanh1865 <hoangducanh.1865@gmail.com>
2026-01-01 04:45:31 +0700

    feat: update paths

--- src/config.py
+++ src/config.py
@@ -1,9 +1,13 @@
+import os
 import torch
 
 
 class Config:
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    dataset = "MNIST"  # Options: "MNIST" or "CelebA"
-    path_to_dataset = "data/CelebA" if dataset == "CelebA" else "data"  # MNIST downloads to data
+    dataset = "MNIST"  # ["MNIST", "CelebA"]
+    path_to_dataset = (
+        os.path("data/CelebA") if dataset == "CelebA" else "data"
+    )  # MNIST downloads to data
     lr = 0.005
     num_epochs = 5
+    path_to_checkpoints = os.path("models/checkpoints")
